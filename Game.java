971d0c2cbca8a7254128062a4b9a9dd93e8f3235
lmaskati <38430340+lmaskati@users.noreply.github.com>
2020-04-20 19:33:56 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class Game {
 
@@ -9,10 +11,56 @@ public class Game {
     }
     public static void main(String args[]) {
         //init players 
-        Human player1 = new Human(true);
-        Human player2 = new Human(false);
-        
+        //Human player1 = new Human(true);
+        Player player1;
+        Player player2;
+
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Animal Chess!");
+
+        System.out.println("If you don't have two players, you can play Animal Chess against the computer.");
+        System.out.println("Would you like to play against the computer? (Y/N)");
+
+        String s = scanner.nextLine();
+        if (s.equals("Y")) {
+            System.out.println("Would you like your difficulty setting to be easy (1), medium (2) or hard (3)? (1/2/3)?");
+            s = scanner.nextLine();
+
+            Random rd = new Random(); 
+           // Boolean boolRand = rd.nextBoolean();
+           Boolean boolRand = true;
+            if (boolRand) {
+                System.out.println("You have randomly been selected to be Player 2.");
+                player1 = new AI(true, false);
+                //set difficulty
+                player1.setLevel(s);
+                player2 = new Human(false, true);
+            }
+            else {
+                System.out.println("You have randomly been selected to be Player 1.");
+                player1 = new Human(true, true);
+                player2 = new AI(false, false);
+                player2.setLevel(s);
+            }
+
+        }
+        else {
+            player1 = new Human(true, true);
+            player2 = new Human(false, true);
+        }
+
+        System.out.println("Would you like to read the instructions for this game? (Y/N)");
+        s = scanner.nextLine();
+
+        if (s.equals("Y"))
+            printRules(newB, player1, player2);
+
+        System.out.println('\n');
+        
+        System.out.println("Press any key to continue.\n");
+        s = scanner.nextLine();
+
         while (gameOn) {
 
             if (!newB.isOn) {
@@ -20,97 +68,57 @@ public class Game {
             }
             
             //player1's turn
-            newB.displayBoard(player1, player2);
-            System.out.println("Player 1, it's your turn!");
-            player1.nextMove(newB, scanner);
+            //newB.displayBoard(player1, player2);
+            //System.out.println("Player 1, it's your turn!");
+            if (player1.human) {
+                newB.displayBoard(player1, player2);
+            }
+
+            player1.nextMove(newB, scanner, player1.getPieces(), player2.getPieces());
+            
+            if (!newB.isOn) {
+                break;
+            }
             //player2's turn
+            //newB.displayBoard(player1, player2);
+           // System.out.println("Player 2's it's your turn!");
+           if (player2.human) {
+            
             newB.displayBoard(player1, player2);
-            System.out.println("Player 2's it's your turn!");
-            player2.nextMove(newB, scanner);
+        }
+            player2.nextMove(newB, scanner, player1.getPieces(), player2.getPieces());
         }
         scanner.close();
         
     }
 
-    // public static void singeMove(Player curPlayer, Scanner scanner) {
+    public static void printRules(Board board, Player player1, Player player2) {
+        System.out.println("INSTRUCTIONS: \n");
+        System.out.println("- Each of the two players has four pieces already placed on the board:");
+        System.out.println("- An elephant (e), a lion (l), a giraffe (g), and a chick (c)\n");
+        System.out.println("- Player 1's pieces are denoted using lowercase, while Player 2's pieces are denoted using uppercase.\n");
+        
+        System.out.println("- The game board is arranged as below. Player 1's pieces are facing the bottom of the board; Player 2's pieces are facing the top of the board:\n");
+        board.displayBoard(player1, player2);
+        System.out.println('\n');
+        
+        System.out.println("- Players take turns moving a single piece:");
+        System.out.println("  1. A chick moves one space forwards.");
+        System.out.println("  2. An elephant moves one space in any diagonal direction (4 possibilities).");
+        System.out.println("  3. A giraffe moves one space in any orthogonal direction (4 possibilities).");
+        System.out.println("  4. A lion moves one space in any direction, either orthogonal or diagonal (8 possibilities).\n");
+    
+        System.out.println("- A player cannot move a piece onto one of their own pieces.");
+        System.out.println("- But they may move onto one of their opponent’s pieces, in which case the opponent’s piece is captured.");
+        System.out.println("- The capturing player takes the piece as one of their 'extra pieces'.");
+        System.out.println("- On a future turn, a player may place an extra piece onto any empty space on the board as one of their own pieces.\n");
+    
+        System.out.println("- A player wins by capturing their opponent’s lion.\n");
 
-    //     //scanner to read user input
+        System.out.println("- Move a piece using the format 'ga2a3'. The order is: piece, starting column, starting row, ending column, ending row.\n");
 
-    //         //get user input
-    //         String s = scanner.nextLine();
+        System.out.println("- If the piece is an extra piece, use the format 'g--c3', where the starting column and starting row are represented by '-'.");
 
-    //         while (true) {
 
-    //             while (s.length() != 5) {
-    //                 System.out.println("Please format your move correctly.");
-    //                 s = scanner.nextLine();
-    //             }
-
-    //             String piece = Character.toString(s.charAt(0));
-    //             String startX = Character.toString(s.charAt(1));
-    //             String startY = Character.toString(s.charAt(2));
-    //             String endX = Character.toString(s.charAt(3));
-    //             String endY = Character.toString(s.charAt(4));
-
-    //             String pieces = "gGcCeElL";
-    //             String xVals = "abc";
-    //             String yVals = "1234";
-
-    //             //check if it is an extra piece
-    //             if ((pieces.contains(piece)) && xVals.contains(endX) && yVals.contains(endY)
-    //             && (startX.equals("-")) && (startX.equals("-"))) {
-    //                 if (newB.moveExtra(curPlayer, piece, convertX(endX), convertY(endY))) {
-    //                     break;
-    //                 }
-    //                 else {
-    //                     s = scanner.nextLine();
-    //                 }
-    //             }
-    //             //check if it a piece on the board
-    //             if ((pieces.contains(piece)) && xVals.contains(startX) && xVals.contains(endX)
-    //             && yVals.contains(startY) && yVals.contains(endY)) {
-    //                 if(newB.makeMove(curPlayer, piece, convertX(startX), convertY(startY), convertX(endX), convertY(endY))) {
-    //                     break;
-    //                 }
-    //                 else {
-    //                     s = scanner.nextLine();
-    //                 }
-    //             }
-
-    //             else {
-    //                 System.out.println("Please choose a valid piece type, row and column.");
-    //                 s = scanner.nextLine();
-    //             }
-    //     }
-    // }
-
-    // public static int convertX(String xVal) {
-    //     if (xVal.equals("a")) {
-    //         return 0;
-    //     }
-    //     if (xVal.equals("b")) {
-    //         return 1;
-    //     }
-    //     if (xVal.equals("c")) {
-    //         return 2;
-    //     }
-    //     return -1;
-    // }
-
-    // public static int convertY(String yVal) {
-    //     if (yVal.equals("1")) {
-    //         return 0;
-    //     }
-    //     if (yVal.equals("2")) {
-    //         return 1;
-    //     }
-    //     if (yVal.equals("3")) {
-    //         return 2;
-    //     }
-    //     if (yVal.equals("4")) {
-    //         return 3;
-    //     }
-    //     return -1;
-    // }
-
+    }
 }
